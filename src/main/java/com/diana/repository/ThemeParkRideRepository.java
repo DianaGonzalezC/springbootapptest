e2edc402c21e6fcc4f2e2d78d2648a8bf1713a5d
DianaGonzalezC <dimarceg@gmail.com>
2021-02-20 13:43:14 -0500

    first commit

---
+++ src/main/java/com/diana/repository/ThemeParkRideRepository.java
@@ -0,0 +1,10 @@
+package com.diana.repository;
+
+import com.diana.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
